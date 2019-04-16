54e0b1eff1ea4f2ad4c1e66e0326c542c536ffe2
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-04-16 15:34:31 +0200

    Finding values in map

--- maps.py
+++ maps.py
@@ -8,4 +8,11 @@ locations['Ásia']['China'] = ['Xangai']
 
 locations['África'] = {'Egito': ['Cairo']}
 
-print(locations)
+cities_of_asia = []
+for countries, cities in locations['Ásia'].items():
+    city_country = cities[0] + " - " + countries
+    cities_of_asia.append(city_country)
+
+
+print(sorted(locations['América do Norte']['Estados Unidos']))
+print(sorted(cities_of_asia))
