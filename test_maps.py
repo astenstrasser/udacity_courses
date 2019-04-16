451ec8cecb855e4d946c7c2e1ab85869ef4d8d62
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-04-16 15:53:26 +0200

    Testing Finding Cities of Usa

--- test_maps.py
+++ test_maps.py
@@ -13,6 +13,10 @@ class TestMaps:
         locations['África'] = {'Egito': ['Cairo']}
         return locations
 
-    def test_cities_of_usa(self, locations):
+    def test_cities_of_asia(self, locations):
         expected = ['Bangalore - Índia', 'Xangai - China']
         assert maps.cities_of_asia(locations) == expected
+
+    def test_cities_of_usa(self, locations):
+        expected = ['Atlanta', 'Mountain View']
+        assert maps.cities_of_usa(locations) == expected
