b59410bdfbfc6a937f7839941c784dccc2d592ab
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-04-16 15:18:09 +0200

    Python Practice - Maps Exercise

---
+++ maps.py
@@ -0,0 +1,11 @@
+# maps exercise - python practice
+
+locations = {'América do Norte': {'Estados Unidos': ['Mountain View']}}
+locations['América do Norte']['Estados Unidos'].append('Atlanta')
+
+locations['Ásia'] = {'Índia': ['Bangalore']}
+locations['Ásia']['China'] = ['Xangai']
+
+locations['África'] = {'Egito': ['Cairo']}
+
+print(locations)
