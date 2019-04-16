beafbc28b4e67e1c99a6bdd0f72740aa119f8873
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-04-16 15:48:52 +0200

    Writing Tests

--- maps.py
+++ maps.py
@@ -8,11 +8,19 @@ locations['Ásia']['China'] = ['Xangai']
 
 locations['África'] = {'Egito': ['Cairo']}
 
-cities_of_asia = []
-for countries, cities in locations['Ásia'].items():
-    city_country = cities[0] + " - " + countries
-    cities_of_asia.append(city_country)
+
+def cities_of_asia(locations):
+    cities_of_asia = []
+    for countries, cities in locations['Ásia'].items():
+        city_country = cities[0] + " - " + countries
+        cities_of_asia.append(city_country)
+    return cities_of_asia
 
 
-print(sorted(locations['América do Norte']['Estados Unidos']))
-print(sorted(cities_of_asia))
+def cities_of_usa(locations):
+    cities_of_usa = sorted(locations['América do Norte']['Estados Unidos'])
+    return cities_of_usa
+
+
+print(cities_of_usa(locations))
+print(cities_of_asia(locations))
---
+++ test_maps.py
@@ -0,0 +1,18 @@
+import maps
+import pytest
+
+
+class TestMaps:
+
+    @pytest.fixture
+    def locations(self):
+        locations = {'América do Norte': {'Estados Unidos': ['Mountain View']}}
+        locations['América do Norte']['Estados Unidos'].append('Atlanta')
+        locations['Ásia'] = {'Índia': ['Bangalore']}
+        locations['Ásia']['China'] = ['Xangai']
+        locations['África'] = {'Egito': ['Cairo']}
+        return locations
+
+    def test_cities_of_usa(self, locations):
+        expected = ['Bangalore - Índia', 'Xangai - China']
+        assert maps.cities_of_asia(locations) == expected
