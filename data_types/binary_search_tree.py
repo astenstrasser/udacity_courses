3f1664c66802181c473bb054544e3943e3348926
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-04-24 18:51:50 +0200

    Implementing Insert Function

--- data_types/binary_search_tree.py
+++ data_types/binary_search_tree.py
@@ -1,5 +1,3 @@
-
-
 class Node:
 
     def __init__(self, value):
@@ -12,11 +10,24 @@ class BinarySearchTree:
     def __init__(self, value):
         self.root = Node(value)
 
-    def insert(self, value):
-        pass
+    def insert(self, value, position = 'root'):
+        if position == 'root':
+            position = self.root
+        if position.value < value:
+            if position.left == None:
+                position.left = Node(value)
+            else:
+                insert(value, position.left)   
+        if position.value > value:
+            if position.right == None:
+                position.right = Node(value)
+            else:
+                insert(value, position.right)
 
-    def get_values(self, position = self.root, tree_values = ''):
+    def get_values(self, position = 'root', tree_values = ''):
         if position:
+            if position == 'root':
+                position = self.root
             tree_values += str(position.value)
             tree_values = get_values(self.left, tree_values)
             tree_values = get_values(self.right, tree_values)
