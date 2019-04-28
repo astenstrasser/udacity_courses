c5dfe852365fa9e5e7ad1b53a2df1c3031b66538
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-04-28 20:40:59 +0200

    Initial commit: Python DB-API class instructions and material

---
+++ relational-databases/forum_db.py
@@ -0,0 +1,11 @@
+import datetime
+
+POSTS = [("This is the first post.", datetime.datetime.now())]
+
+def get_posts():
+  """Return all posts from the 'database', most recent first."""
+  return reversed(POSTS)
+
+def add_post(content):
+  """Add a post to the 'database' with the current timestamp."""
+  POSTS.append((content, datetime.datetime.now()))
