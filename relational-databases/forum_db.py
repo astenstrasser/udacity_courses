e14f5e9b335ce1206d4986b726c641e83f01d0aa
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-05-07 09:48:38 +0200

    Updating table to remove spam

--- relational-databases/forum_db.py
+++ relational-databases/forum_db.py
@@ -2,6 +2,7 @@
 
 import datetime
 import psycopg2
+import bleach
 
 POSTS = [("This is the first post.", datetime.datetime.now())]
 
@@ -9,6 +10,7 @@ POSTS = [("This is the first post.", datetime.datetime.now())]
 def get_posts():
     data_base = psycopg2.connect("dbname=forum")
     cursor = data_base.cursor()
+    cursor.execute("update posts set content = 'safe content, not spam!' where content like '%spam%'")
     cursor.execute('select content, time from posts order by time desc')
     POSTS = cursor.fetchall()
     data_base.close()
@@ -18,6 +20,7 @@ def get_posts():
 def add_post(content):
     data_base = psycopg2.connect("dbname=forum")
     cursor = data_base.cursor()
-    cursor.execute('insert into posts values (%s)', (content,)) #important to protect code from SQL injections!
+    # important to protect code from SQL injections!
+    cursor.execute('insert into posts values (%s)', (content,))
     data_base.commit()
     data_base.close()
