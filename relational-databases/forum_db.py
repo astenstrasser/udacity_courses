5053035b04d1725f0dc993946c23893ab19f1d3d
Alexandra Monteiro Stenstrasser <astenstrasser@gmail.com>
2019-05-07 10:10:51 +0200

    Using bleach library to avoid spam and sql injections

--- relational-databases/forum_db.py
+++ relational-databases/forum_db.py
@@ -11,8 +11,8 @@ def get_posts():
     data_base = psycopg2.connect("dbname=forum")
     cursor = data_base.cursor()
     cursor.execute("update posts set content = 'this_was_spam' where content like '%spam%'")
-    cursor.execute("delete from posts where content = 'this_was_spam'")
-    cursor.execute('select content, time from posts order by time desc')
+    cursor.execute("delete from posts where content = 'this_was_spam' or content = ''")
+    cursor.execute("select content, time from posts order by time desc")
     POSTS = cursor.fetchall()
     data_base.close()
     return POSTS
@@ -21,7 +21,6 @@ def get_posts():
 def add_post(content):
     data_base = psycopg2.connect("dbname=forum")
     cursor = data_base.cursor()
-    # important to protect code from SQL injections!
-    cursor.execute('insert into posts values (%s)', (content,))
+    cursor.execute("insert into posts values (%s)", (bleach.clean(content),) )  # important to use bleach to protect code from SQL injections!
     data_base.commit()
     data_base.close()
